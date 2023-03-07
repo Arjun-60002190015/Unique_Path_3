dd45a6f8a4732ffd774e662a5a690baddcc89cd8
dell <arjunpathak164@gmail.com>
2023-03-07 22:14:58 +0530

    f

--- src/Integer/practiceQuestion/books.java
+++ src/Integer/practiceQuestion/books.java
@@ -1,22 +1,8 @@
 package Integer.practiceQuestion;
 
 
-import java.util.LinkedList;
-import java.util.Queue;
-
 public class books{
-    public boolean canReach(int[] arr, int start) {
-        Queue<Integer> q = new LinkedList<>();
-        q.offer(start);
-        while(!q.isEmpty()){
-            start = q.poll();
-            if(arr[start]==0)
-                return true;
-            if(arr[start]+ start<arr.length)
-                q.add(arr[start] + start);
-            if
-        }
-    }
+
 
 
 
